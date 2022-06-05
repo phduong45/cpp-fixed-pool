c850ebe6b26ec4e5983bed94073db0ea8c02de2a
Phuong Duong <phuongx31@gmail.com>
2022-06-05 15:16:45 +0700

    place objects into manual slots

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstddef>
 #include <iostream>
 #include <new>
@@ -23,7 +24,7 @@ int main() {
     {
         // This is raw storage only. No User object is alive yet.
         alignas(User) std::byte buffer[sizeof(User)];
-        User* p = new (buffer) User("placement");
+        User* p = new (buffer) User{"placement"};
 
         // Placement new does not own the storage, so we destroy explicitly.
         p->~User();
@@ -31,13 +32,36 @@ int main() {
 
     {
         alignas(User) std::byte buffer[sizeof(User)];
-        User* first = new (buffer) User("first");
+        User* first = new (buffer) User{"first"};
         std::cout << "address of first: " << static_cast<void*>(first) << "\n";
         first->~User();
 
-        User* second = new (buffer) User("second");
-        std::cout << "address of second: " << static_cast<void*>(second) << "\n";
+        User* second = new (buffer) User{"second"};
+        std::cout << "address of second: " << static_cast<void*>(second)
+                  << "\n";
         second->~User();
+    }
+
+    {
+        alignas(User) std::byte storage[2 * sizeof(User)];
+        std::byte* slot0 = storage + 0 * sizeof(User);
+        std::byte* slot1 = storage + 1 * sizeof(User);
+
+        User* a = new (slot0) User{"slot0"};
+        User* b = new (slot1) User{"slot1"};
+
+        std::cout << "address of a: " << static_cast<void*>(a) << "\n";
+        std::cout << "address of b: " << static_cast<void*>(b) << "\n";
+
+        auto distance =
+            reinterpret_cast<std::byte*>(b) - reinterpret_cast<std::byte*>(a);
+
+        assert(distance == sizeof(User));
+        std::cout << "distance: " << distance << "\n";
+        std::cout << "sizeof(User): " << sizeof(User) << "\n";
+
+        b->~User();
+        a->~User();
     }
 
     return 0;
